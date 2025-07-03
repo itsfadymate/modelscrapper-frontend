10e1540353325eb463385fb9027b8e38f6218114
itsfadymate <fadybassem263@gmail.com>
2025-07-03 12:09:51 +0300

    tested gifs

--- scrapper-view/src/App.jsx
+++ scrapper-view/src/App.jsx
@@ -42,14 +42,14 @@ function App() {
         },
         {
           id: 5,
-          imagelink: "https://cdn.thingiverse.com/renders/2f/93/ac/61/61/photo_preview_card.jpg",
-          modelname: `Elliptical Gear with no center pivot`,
-          websitename: "Thingiverse",
-          websitelink: "https://www.thingiverse.com/thing:1082508"
+          imagelink: "https://videos.cults3d.com/dgtQk4mNy5oa-C0lKCXOWmogUQ8=/246x246/filters:no_upscale()/https://fbi.cults3d.com/uploaders/17407681/illustration-file/89d83981-1a5b-4fa9-99bd-c7dc7b4e21f2/20220618_145209.gif",
+          modelname: `Random Dragon`,
+          websitename: "Cults3D",
+          websitelink: "https://cults3d.com/en/3d-model/art/dragon-canvas3digital"
         },
         {
           id: 6,
-          imagelink: "https://cdn.thingiverse.com/renders/2f/93/ac/61/61/photo_preview_card.jpg",
+          imagelink: "https://cdn.thingiverse.com/renders/93/de/40/de/31/6af47985fd58a3938d7615f77249a8d4_preview_card.jpg",
           modelname: `Elliptical Gear Set with connecting links.`,
           websitename: "Thingiverse",
           websitelink: "https://www.thingiverse.com//thing:37659"
@@ -62,7 +62,7 @@ function App() {
         },
         {
           id: 8,
-          imagelink: "https://cdn.thingiverse.com/renders/2f/93/ac/61/61/photo_preview_card.jpg",
+          imagelink: "https://cdn.thingiverse.com/renders/b0/f5/a6/01/ab/IMAG4155_preview_card.jpg",
           modelname: `Elliptical Gear Set with connecting links.`,
           websitename: "Thingiverse",
           websitelink: "https://www.thingiverse.com//thing:37659"
